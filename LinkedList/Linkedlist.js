877cf729d2ccdfa326216e8cc25c0e15f7f9f770
wolecodes <opeyemioyeyemi141@gmail.com>
2025-01-13 01:51:42 +0100

    2623. Memoize

--- LinkedList/Linkedlist.js
+++ LinkedList/Linkedlist.js
@@ -53,3 +53,4 @@ class LinkedList {
 }
 
 module.exports = LinkedList;
+
--- smash.js
+++ smash.js
@@ -1,26 +1,26 @@
-Sentence Smash
-Write a function that takes an array of words and smashes them together into a sentence 
-and returns the sentence. You can ignore any need to sanitize words or add punctuation,
-but you should add spaces between each word. Be careful, 
-there shouldn't be a space at the beginning or the end of the sentence!
+// Sentence Smash
+// Write a function that takes an array of words and smashes them together into a sentence
+// and returns the sentence. You can ignore any need to sanitize words or add punctuation,
+// but you should add spaces between each word. Be careful,
+// there shouldn't be a space at the beginning or the end of the sentence!
 
-### Example
-['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+// ### Example
+// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
 
-solution
+solution;
 
-function smash (words) {
-  let str = '';
-  for(let word of words){
-    if(words.length === 1){
+function smash(words) {
+  let str = "";
+  for (let word of words) {
+    if (words.length === 1) {
       str += word;
-    }
-    else if(!words.length -1 ){
-      str +=  word + " ";
+    } else if (!words.length - 1) {
+      str += word + " ";
     }
   }
   return str;
 }
 
-words = ['hello', 'world', 'this', 'is', 'great']
-smash(words) # returns "hello world this is great"
+words = ["hello", "world", "this", "is", "great"];
+smash(words);
+//# returns "hello world this is great"
